4b5cde9e388c12291796ddc8ef903263e17a91fb
tdabek <dabek.t@gmail.com>
2019-12-30 19:01:03 +0100

    Poprawka formatowania

--- 2.cpp
+++ 2.cpp
@@ -8,75 +8,82 @@ double czas;
 
 double bubble(int a[])
 {
-   int i, j,temp,pass=0;
-   for(i = 0; i<10; i++) {
-      cout <<a[i]<<"\t";
-   }
-cout<<endl;
-for(i = 0; i<10; i++) {
-   for(j = i+1; j<10; j++)
-   {
-      if(a[j] < a[i]) {
-         temp = a[i];
-         a[i] = a[j];
-         a[j] = temp;
-      }
-   }
-pass++;
-return 0;
-}
-void quick_sort(int *tab, int lewy, int prawy)
-{
-	if(prawy <= lewy) return;
-	
-	int i = lewy - 1, j = prawy + 1, 
-	pivot = tab[(lewy+prawy)/2]; 
-	
-	while(1)
+	int i, j, temp, pass = 0;
+	for (i = 0; i < 10; i++)
 	{
-		while(pivot>tab[++i]);
-		
-		while(pivot<tab[--j]);
-		
-		if( i <= j)
+		cout << a[i] << "\t";
+	}
+	cout << endl;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			if (a[j] < a[i])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+		pass++;
+		return 0;
+	}
+}
+void quick_sort(int *tab, int left, int right)
+{
+	if (right <= left)
+		return;
 
-			swap(tab[i],tab[j]);
+	int i = left - 1, j = right + 1,
+		pivot = tab[(left + right) / 2];
+
+	while (1)
+	{
+		while (pivot > tab[++i])
+			;
+
+		while (pivot < tab[--j])
+			;
+
+		if (i <= j)
+
+			swap(tab[i], tab[j]);
 		else
 			break;
 	}
 
-	if(j > lewy)
-	quick_sort(tab, lewy, j);
-	if(i < prawy)
-	quick_sort(tab, i, prawy);
+	if (j > left)
+		quick_sort(tab, left, j);
+	if (i < right)
+		quick_sort(tab, i, right);
 }
-
-
 
 int main()
 {
-srand( time( NULL ) );
+	srand(time(NULL));
 
-int *tab, n;
+	int *tab, n = 10;
 
-tab = new int [rand()]; 
+	tab = new int[n];
 
-  	for(int i=0;i<n;i++)
-    	cin>>tab[i];
-    		start = clock();
-  	quick_sort(tab,0, n-1);
-			czas = (stop-start)/double(CLOCKS_PER_SEC);
-			cout << "czas bubble = " << czas << endl;
-  	cin.ignore();
-  	cin.get();
-delete [] tab;
-tab = new int [rand()]; 
-    		start = clock();
-bubble(tab);
-stop = clock();
-			czas = (stop-start)/double(CLOCKS_PER_SEC);
-			cout << "czas bubble = " << czas << endl;
-delete [] tab;
- 
-return 0;
+	for (int i = 0; i < n; i++)
+	{
+		tab[i] = rand() % 101;
+	}
+
+	start = clock();
+	quick_sort(tab, 0, n - 1);
+	czas = (stop - start) / double(CLOCKS_PER_SEC);
+	cout << "czas bubble = " << czas << endl;
+	cin.ignore();
+	cin.get();
+	delete[] tab;
+	tab = new int[rand()];
+	start = clock();
+	bubble(tab);
+	stop = clock();
+	czas = (stop - start) / double(CLOCKS_PER_SEC);
+	cout << "czas bubble = " << czas << endl;
+	delete[] tab;
+	return 0;
 }
